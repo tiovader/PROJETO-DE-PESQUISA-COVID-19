0653e864688205a891361fad03f474e436c2f79c
Emanuel Luis <emanuel.filho08@gmail.com>
2022-06-14 23:04:08 -0300

    fixing csv lenght to fit github

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_path(row: pd.DataFrame):
 
 
 def main():
-    metadata = pd.read_csv("./csv/metadata.csv")
+    metadata = pd.read_csv("./images/metadata.csv")
     metadata["path"] = metadata.apply(get_path, axis=1)
     metadata.drop(columns=["folder"], inplace=True)
 
@@ -18,7 +18,10 @@ def main():
     to_drop = ["filename", "dataset", "path"]
     columns = df.columns.difference(to_drop).to_list()
     df.drop_duplicates(subset=columns, inplace=True)
-    df.to_csv("./csv/features.csv", index=False)
+    
+    mid = df.shape[0] // 2
+    df.iloc[:mid].to_csv('./csv/features_1.csv', index=False)
+    df.iloc[mid:].to_csv('./csv/features_2.csv', index=False)
 
 
 if __name__ == "__main__":
